951de02312e5c8a86fb49fceb2cfe07c83de3f6e
Rifat Zahan <26659769+rifatzahan@users.noreply.github.com>
2020-09-15 13:09:21 -0600

    Update multicollinearity.R

--- multicollinearity.R
+++ multicollinearity.R
@@ -5,10 +5,8 @@
 library(carData)
 library(car)
 
-vif(glm(status2~ District + education + religion + wealth + residence + totalChildrenEverBorn +
-        cohabAge + maritalStatus + occupation + occupation_partner + education_partner + 
+vif(glm(status2~ District + education + religion + wealth + residence + totalChildrenEverBorn + cohabAge + maritalStatus + occupation + occupation_partner + education_partner + 
  aliveChildren, data=dfaddress2))
 
-vif(glm(status2~ totalChildrenEverBorn + wealth + occupation + 
-          maritalStatus + cohabAge + residence, data=dfaddress2))
+vif(glm(status2~ totalChildrenEverBorn + wealth + occupation + maritalStatus + cohabAge + residence, data=dfaddress2))
 
