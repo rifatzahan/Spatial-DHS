5f9a8b0233bc40be203adf13f4633363d3202fc0
Rifat Zahan <26659769+rifatzahan@users.noreply.github.com>
2020-09-15 13:08:52 -0600

    Check multicollinearity

---
+++ multicollinearity.R
@@ -0,0 +1,14 @@
+###############################
+### Check Multicollinearity ###
+###############################
+
+library(carData)
+library(car)
+
+vif(glm(status2~ District + education + religion + wealth + residence + totalChildrenEverBorn +
+        cohabAge + maritalStatus + occupation + occupation_partner + education_partner + 
+ aliveChildren, data=dfaddress2))
+
+vif(glm(status2~ totalChildrenEverBorn + wealth + occupation + 
+          maritalStatus + cohabAge + residence, data=dfaddress2))
+
